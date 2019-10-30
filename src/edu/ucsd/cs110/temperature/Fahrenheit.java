dd0f7f37cb904a44e6e9375c96e256bba9680382
Paul Zhu <zhz433@ucsd.edu>
2019-10-30 12:44:02 -0700

    initial commit

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,25 @@
+package edu.ucsd.cs110.temperature;
+
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float cVal = (this.getValue() - 32) * 5 / 9;
+        return new Celsius(cVal);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return this;
+    }
+
+    public String toString()
+    {
+        return this.getValue() + " F";
+    }
+}
